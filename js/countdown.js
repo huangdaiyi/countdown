39838e59ba82039cb7c531b531003108bf946ae1
hardy <416788139@qq.com>
2016-05-15 17:40:29 +0800

    hidden day when value is 0

--- js/countdown.js
+++ js/countdown.js
@@ -4,19 +4,20 @@
             var localNow = new Date().getTime();
             options = $.extend({
                 startTime: localNow,
-                endTime: localNow + 60*60*24*1000,
+                endTime: localNow + 60*60*24*1000*1,
                 speed:1000,
                 text:{
                     day:"天",
-                    hour:"小时",
-                    minute:"分钟",
+                    hour:"时",
+                    minute:"分",
                     second:"秒"
                 },
                 totalTime:function(){
                     var total = this.endTime - this.startTime
                     return total < 0 ? 0 : total;
                 },
-                wrap:"span"
+                wrap:"span",
+                descWrap:"span"
             }, options);
 
             var formatTime = function (totalSeconds, outerWrap){
@@ -26,33 +27,56 @@
                  minuWrap  = document.createElement(options.wrap),
                  secoWrap  = document.createElement(options.wrap);
                 outerWrap.appendChild(dayWrap);
-                outerWrap.appendChild(document.createTextNode(desc.day));
+                outerWrap.appendChild(createDescWrap(desc.day));
                 outerWrap.appendChild(hourWrap);
-                outerWrap.appendChild(document.createTextNode(desc.hour));
+                outerWrap.appendChild(createDescWrap(desc.hour));
                 outerWrap.appendChild(minuWrap);
-                outerWrap.appendChild(document.createTextNode(desc.minute));
+                outerWrap.appendChild(createDescWrap(desc.minute));
                 outerWrap.appendChild(secoWrap);
-                outerWrap.appendChild(document.createTextNode(desc.second));
+                outerWrap.appendChild(createDescWrap(desc.second));
 
                 var updateTime = function(totalSeconds){
-                    dayWrap.innerHTML = parseInt(totalSeconds/(60*60*24)) + "";
+                    updateWrapValue(dayWrap, parseInt(totalSeconds/(60*60*24)));
                     var restHours = totalSeconds % (60*60*24);
-                    hourWrap.innerHTML= parseInt(restHours/(60*60)) + "";
+                    updateWrapValue(hourWrap, parseInt(restHours/(60*60)));
                     var restMinus = restHours % (60*60);
-                    minuWrap.innerHTML= parseInt(restMinus/60) + "";
-                    secoWrap.innerHTML= restMinus%60 + "";
-                    
+                    minuWrap.innerHTML= zeroFill(parseInt(restMinus/60));
+                    secoWrap.innerHTML =  zeroFill(restMinus%60);
                     setTimeout(function(){
-                        updateTime(totalSeconds - opts.speed/1000);
+                        var restSeconds = totalSeconds - opts.speed/1000;
+                        if(restSeconds > 0){
+                            updateTime(restSeconds);
+                        }else {
+                            //TODO:clear
+                        }
+
                     }, opts.speed);
                 };
                 updateTime(totalSeconds);
             };
 
             return this.each(function(){
-              formatTime(options.totalTime()/1000, this);
+              formatTime(parseInt(options.totalTime()/1000), this);
             });
-        };
 
-   
+            function updateWrapValue(wrap, value) {
+                var prev = wrap.previousElementSibling;
+                if(value < 1 && (!prev || prev.style.display !== "none")){
+                    wrap.style.display = "none";
+                    wrap.nextElementSibling.style.display = "none";
+                }else {
+                    wrap.innerHTML= zeroFill(value);
+                }
+            };
+
+            function createDescWrap(desc) {
+                var temp = document.createElement(options.descWrap);
+                temp.innerHTML = desc;
+                return temp;
+            };
+
+            function zeroFill(intValue){
+            	return intValue < 10 ? "0"+ intValue: intValue + ""
+            }
+        };
       })(jQuery);
